dd7346fe2dd862592af7aa8cfc57d8a84c320e8d
Ayelet <ayelet203320@gmail.com>
2024-05-26 17:25:58 +0300

    Change functions to async/await

--- app/db_manegement/repositories/user_repository.py
+++ app/db_manegement/repositories/user_repository.py
@@ -1,30 +1,27 @@
 from ..connection import get_database
 from ...models.user import User
 from bson import ObjectId
-from typing import List
 
 db = get_database()
 users_collection = db["users"]
 
 
-def create_user(user: User) -> ObjectId:
-    result = users_collection.insert_one(user.dict())
-    return result.inserted_id
+async def create_user(user: User):
+    user_dict = user.dict()
+    result = await users_collection.insert_one(user_dict)
+    user.id = str(result.inserted_id)
+    await users_collection.update_one({'_id': result.inserted_id}, {'$set': {'id': user.id}})
+    return user
 
 
-def get_user_by_id(user_id: ObjectId) -> User:
-    user_data = users_collection.find_one({"_id": user_id})
+async def get_user_by_id(user_id: ObjectId) -> User:
+    user_data = await users_collection.find_one({"_id": user_id})
     return User(**user_data) if user_data else None
 
 
-def update_user(user_id: ObjectId, user: User):
-    users_collection.update_one({"_id": user_id}, {"$set": user.dict()})
+async def update_user(user_id: ObjectId, user: User):
+    await users_collection.update_one({"_id": user_id}, {"$set": user.dict()})
 
 
-def delete_user(user_id: ObjectId):
-    users_collection.delete_one({"_id": user_id})
-
-
-# def get_all_users() -> List[User]:
-#     user_data = users_collection.find({})
-#     return [User(**user) for user in user_data]
+async def delete_user(user_id: ObjectId):
+    await users_collection.delete_one({"_id": user_id})
